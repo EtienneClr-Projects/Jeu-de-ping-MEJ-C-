ff759a1453c1f52f967a5caa4f9e2d4c1960c97b
EtienneClrProjets <60063320+UTTprojetsEtienne@users.noreply.github.com>
2022-03-04 20:13:49 +0100

    first commit

---
+++ Ping.cpp
@@ -0,0 +1,19 @@
+//
+// Created by etienne on 04/03/2022.
+//
+
+#include "Ping.h"
+
+#define n 4
+
+int *generate_sol_init() {
+//    n=3;
+    int combs[n * n][n] = generate_combinations_with_replacement();
+    int combs_non_duplicates[n * n][n] = {};
+
+
+}
+
+int *generate_combinations_with_replacement() {
+    return new int[10];
+}
---
+++ Ping.h
@@ -0,0 +1,12 @@
+//
+// Created by etienne on 04/03/2022.
+//
+
+#ifndef JEUDEPINGMEJ_PING_H
+#define JEUDEPINGMEJ_PING_H
+
+int * generate_combinations_with_replacement();
+int * generate_sol_init();
+
+
+#endif //JEUDEPINGMEJ_PING_H
